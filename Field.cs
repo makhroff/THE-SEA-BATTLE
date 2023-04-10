54cd8330bffd0bbe5c431e774e441de5472637d8
FeM <philippe.makhroff@gmail.com>
2023-04-10 19:53:12 +0200

    new classes

---
+++ Field.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sea_battle
+{
+    public class Field
+    {
+        private Cell[,] cells;
+
+        public Field(int size) 
+        { 
+            cells = new Cell[size, size * 2];
+        }
+    }
+}
